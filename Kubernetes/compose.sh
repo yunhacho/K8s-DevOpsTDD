b92cd7bbb1bc383aba3da6b8b5b63bfbc8a0cc33
yunhacho <jyh_5530@naver.com>
2021-09-05 20:58:08 +0900

    Kubernetes compose

--- Kubernetes/compose.sh
+++ Kubernetes/compose.sh
@@ -1,9 +1,15 @@
-kubectl delete ns dev
-kubectl delete ns elastic
-kubectl delete pv es-dev-pv-volume
-kubectl delete pv es-pv-volume
+# kubectl delete ns dev
+# kubectl delete ns elastic
+# kubectl delete pv es-dev-pv-volume
+# kubectl delete pv es-pv-volume
 kubectl create ns dev
+kubectl create ns deploy
 kubectl create ns elastic
-kubectl apply -f elastic_pv.yaml
-kubectl apply -f elastic_compose.yaml
-helm install wepapp github-test/test --set service.nodePort=30001 --set ElasticSearch=elastic-dev-svc.dev.svc.cluster.local -n dev
+kubectl apply -f Elasticsearch-pv.yaml
+kubectl apply -f Elasticsearch-compose.yaml
+kubectl apply -f jenkins-pv.yaml
+kubectl apply -f jenkins-compose.yaml
+docker compose -f gitlab-docker-compose.yaml -d
+
+# For dev namespace
+helm install wepapp github-webanalysis/webanalysis --set service.nodePort=30001 --set ElasticSearch=elastic-dev-svc.dev.svc.cluster.local -n dev
